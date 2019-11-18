b2d86aae7c0c0aa180036215729c872aaa2eaa16
aj <ajshanthappa@gmail.com>
2019-11-18 14:07:31 +0000

    added Homepage, Articles, Topics and Users components

--- nc-news/src/App.js
+++ nc-news/src/App.js
@@ -1,11 +1,18 @@
 import React from "react"
 import "./App.css"
+import { Router } from "@reach/router"
 import Header from "./components/Header.jsx"
 
-function App () {
+function App() {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Homepage />
+        <Users />
+        <Articles />
+        <Topics />
+      </Router>
     </div>
   )
 }
