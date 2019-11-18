7befb5ebb256765d00adebd53d2934af6450288c
aj <ajshanthappa@gmail.com>
2019-11-18 15:32:57 +0000

    topics rendering working

--- nc-news/src/api.js
+++ nc-news/src/api.js
@@ -7,3 +7,11 @@ export const getUsers = () => {
       return res.data.users
     })
 }
+
+export const getTopics = () => {
+  return axios
+    .get("https://news-website-aj.herokuapp.com/api/topics")
+    .then(res => {
+      return res.data.topics
+    })
+}
---
+++ nc-news/src/components/Topics.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import * as api from "../api.js"
+
+class Topics extends React.Component {
+  state = {
+    topics: [],
+    isLoading: true
+  }
+
+  render() {
+    const { topics, isLoading } = this.state
+    if (isLoading) return <p>Loading...</p>
+    return (
+      <ul>
+        {topics.map(topic => {
+          return <li>{topic.slug}</li>
+        })}
+      </ul>
+    )
+  }
+  componentDidMount() {
+    api.getTopics().then(topics => {
+      this.setState({ topics, isLoading: false })
+    })
+  }
+}
+
+export default Topics
