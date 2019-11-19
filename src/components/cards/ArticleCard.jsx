4a3af1664468473e199b0fd833bdcf3e227e86d7
aj <ajshanthappa@gmail.com>
2019-11-19 12:02:58 +0000

    added card functionality to articles

--- src/components/cards/ArticleCard.jsx
+++ src/components/cards/ArticleCard.jsx
@@ -2,19 +2,21 @@ import React from "react"
 
 const ArticleCard = props => {
   const {
-    article_id,
     title,
     author,
     topic,
     created_at,
-    votes,
-    comment_count
+    votes
   } = props.article
 
-  console.log(props)
+  console.log(props.article)
   return(
     <main>
       <h2>{title}</h2>
+      <h3>{topic}</h3>
+      <h3>{author}</h3>
+      <h5>{votes}</h5>
+      <h5>{created_at}</h5>
     </main>
   )
 }
