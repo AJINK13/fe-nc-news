cf20ba73f8397de0f91b9393804c6b188e6f516f
aj <ajshanthappa@gmail.com>
2019-11-19 15:04:55 +0000

    used template literal for baseURL

--- src/api.js
+++ src/api.js
@@ -1,25 +1,21 @@
 import axios from "axios"
 
+const baseURL = "https://news-website-aj.herokuapp.com/api"
+
 export const getUsers = () => {
-  return axios
-    .get("https://news-website-aj.herokuapp.com/api/users")
-    .then(res => {
-      return res.data.users
-    })
+  return axios.get(`${baseURL}/users`).then(res => {
+    return res.data.users
+  })
 }
 
 export const getTopics = () => {
-  return axios
-    .get("https://news-website-aj.herokuapp.com/api/topics")
-    .then(res => {
-      return res.data.topics
-    })
+  return axios.get(`${baseURL}/topics`).then(res => {
+    return res.data.topics
+  })
 }
 
 export const getArticles = () => {
-  return axios
-    .get("https://news-website-aj.herokuapp.com/api/articles")
-    .then(res => {
-      return res.data.articles
-    })
+  return axios.get(`${baseURL}/articles`).then(res => {
+    return res.data.articles
+  })
 }
--- src/components/cards/ArticleCard.jsx
+++ src/components/cards/ArticleCard.jsx
@@ -1,16 +1,25 @@
 import React from "react"
+import { Link } from "@reach/router"
 const moment = require("moment")
 
 const ArticleCard = props => {
-  const { title, author, topic, created_at, votes } = props.article
+  const { title, author, topic, created_at, votes, article_id } = props.article
+  console.log(props)
 
   return (
     <main>
-      <h2>{title}</h2>
+      {/* <nav>
+        <Link to="/articles/:article_id"> */}
+          <h2>{title}</h2>
+        {/* </Link>
+      </nav> */}
       <h3>Topic: {topic}</h3>
       <h3>Author: {author}</h3>
-      <h5> Votes: {votes}</h5>
-      <h5>Date Created: {moment(created_at).format("dddd, MMMM Do YYYY, h:mm:ss a")}</h5>
+      <h5>Votes: {votes}</h5>
+      <h5>
+        Date Created:{" "}
+        {moment(created_at).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+      </h5>
     </main>
   )
 }
