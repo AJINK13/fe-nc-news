a437fed2c2024acc0302de8779ba195337e48bab
aj <ajshanthappa@gmail.com>
2019-11-20 10:59:36 +0000

    removed heading from single article comment card

--- src/components/cards/SingleArticleCommentCard.jsx
+++ src/components/cards/SingleArticleCommentCard.jsx
@@ -6,10 +6,9 @@ const SingleArticleCommentCard = props => {
 
   return (
     <main>
-      <h2>Article Comments</h2>
       <h3>Author: {author}</h3>
       <h4>{body}</h4>
-      <h6>Votes: {votes}</h6>
+      <h5>Votes: {votes}</h5>
       <h6>
         Date Created:{" "}
         {moment(created_at).format("dddd, MMMM Do YYYY, h:mm:ss a")}
