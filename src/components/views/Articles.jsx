bcd68613161195b32c9a177fef3bac5529720ada
aj <ajshanthappa@gmail.com>
2019-11-22 09:37:13 +0000

    added error page component to articles

--- src/components/views/Articles.jsx
+++ src/components/views/Articles.jsx
@@ -2,15 +2,18 @@ import React from "react"
 import * as api from "../../api.js"
 import ArticleCard from "../cards/ArticleCard.jsx"
 import SortArticles from "../views/SortArticles.jsx"
+import ErrorPage from "./ErrorPage.jsx"
 
 class Articles extends React.Component {
   state = {
     articles: [],
-    isLoading: true
+    isLoading: true,
+    error: null
   }
 
   render() {
-    const { articles, isLoading } = this.state
+    const { articles, isLoading, error } = this.state
+    if (error) return <ErrorPage error={error} />
     if (isLoading) return <p>Loading...</p>
     return (
       <main>
@@ -35,9 +38,20 @@ class Articles extends React.Component {
 
   fetchArticles = (sort_by, order) => {
     const { topic } = this.props
-    api.getArticles(topic, sort_by, order).then(articles => {
-      this.setState({ articles, isLoading: false })
-    })
+    api
+      .getArticles(topic, sort_by, order)
+      .then(articles => {
+        this.setState({ articles, isLoading: false })
+      })
+      .catch(err => {
+        this.setState({
+          error: {
+            status: err.response.status,
+            message: err.response.data.msg
+          },
+          isLoading: false
+        })
+      })
   }
 }
 
