8c583f09a8167c8148df20ed6ca9a1a9eb89d585
aj <ajshanthappa@gmail.com>
2019-11-22 09:52:37 +0000

    added error page to users

--- src/components/views/Users.jsx
+++ src/components/views/Users.jsx
@@ -1,16 +1,21 @@
 import React from "react"
 import * as api from "../../api.js"
 import UserCard from "../cards/UserCard.jsx"
+import ErrorPage from "./ErrorPage.jsx"
 
 class Users extends React.Component {
   state = {
     users: [],
-    isLoading: true
+    isLoading: true,
+    error: null
   }
 
   render() {
-    const { users, isLoading } = this.state
+    const { users, isLoading, error } = this.state
+
+    if (error) return <ErrorPage error={error} />
     if (isLoading) return <p>Loading...</p>
+
     return (
       <main>
         <h2>Users</h2>
@@ -28,9 +33,20 @@ class Users extends React.Component {
   }
 
   fetchUsers = () => {
-    api.getUsers().then(users => {
-      this.setState({ users, isLoading: false })
-    })
+    api
+      .getUsers()
+      .then(users => {
+        this.setState({ users, isLoading: false })
+      })
+      .catch(err => {
+        this.setState({
+          error: {
+            status: err.response.status,
+            message: err.response.data.msg
+          },
+          isLoading: false
+        })
+      })
   }
 }
 
