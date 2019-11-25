5b6fabd072ededaa4abcb33b8684492aaeecc467
aj <ajshanthappa@gmail.com>
2019-11-25 14:03:57 +0000

    added delete icon

--- src/components/views/RemoveComment.jsx
+++ src/components/views/RemoveComment.jsx
@@ -1,13 +1,19 @@
 import React from "react"
+import Button from "@material-ui/core/Button"
+import DeleteIcon from "@material-ui/icons/Delete"
 
 const RemoveComment = props => {
   const { removeCommentByCommentID, comment_id } = props
   return (
-    <button
+    <Button
+      variant="contained"
+      startIcon={<DeleteIcon />}
       onClick={() => {
         removeCommentByCommentID(comment_id)
       }}
-    >DELETE COMMENT</button>
+    >
+      DELETE COMMENT
+    </Button>
   )
 }
 
