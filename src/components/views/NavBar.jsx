980c40f3d04aad52dfb1b7904ce116ec5f2c0492
Ajinkya <ajinkya1394@outlook.com>
2019-12-20 00:37:47 +0000

    add styling to navbar

--- src/components/views/NavBar.jsx
+++ src/components/views/NavBar.jsx
@@ -1,7 +1,16 @@
 import React from "react"
 
 const NavBar = () => {
-  return <h1>Nav Bar</h1>
+  return (
+    <div className="topnav">
+      <a className="active" href="/">
+        Homepage
+      </a>
+      <a href="/articles">Articles</a>
+      <a href="/topics">Topics</a>
+      <a href="/users">Users</a>
+    </div>
+  )
 }
 
 export default NavBar
