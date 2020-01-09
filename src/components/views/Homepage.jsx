eb828cebc519e530536828c39ab7b6f2e1b27edb
Ajinkya <ajinkya1394@outlook.com>
2020-01-09 03:26:00 +0000

    add homepage view

--- src/components/views/Homepage.jsx
+++ src/components/views/Homepage.jsx
@@ -1,7 +1,20 @@
 import React from "react"
 
 function Homepage() {
-  return <main>Homepage</main>
+  return (
+    <div className="homepage">
+      <div className="container">
+        <div className="row">
+          <div className="homepage-content">
+            <div className="welcome-message">
+              <h3>Welcome to</h3>
+              <h1>News Concept</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Homepage
