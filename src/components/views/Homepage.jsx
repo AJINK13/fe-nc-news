1fd8589daa480b710634b41599ad461f1a4f0398
Ajinkya <ajinkya1394@outlook.com>
2020-01-09 04:28:01 +0000

    remove container from components

--- src/components/views/Homepage.jsx
+++ src/components/views/Homepage.jsx
@@ -3,12 +3,10 @@ import React from "react"
 function Homepage() {
   return (
     <div className="homepage">
-      <div className="container">
-        <div className="homepage-content">
-          <div className="welcome-message">
-            <h3>Welcome to</h3>
-            <h1>News Concept</h1>
-          </div>
+      <div className="homepage-content">
+        <div className="welcome-message">
+          <h3>Welcome to</h3>
+          <h1>News Concept</h1>
         </div>
       </div>
     </div>
--- src/components/views/NavBar.jsx
+++ src/components/views/NavBar.jsx
@@ -2,20 +2,20 @@ import React from "react"
 
 const NavBar = () => {
   return (
-    <div className="topnav">
-      <a href="/">
-        <i class="fas fa-home"></i> Homepage
-      </a>
-      <a href="/articles">
-        <i class="fas fa-newspaper"></i> Articles
-      </a>
-      <a href="/topics">
-        <i class="fas fa-book"></i> Topics
-      </a>
-      <a href="/users">
-        <i class="fas fa-users"></i> Users
-      </a>
-    </div>
+    <header>
+        <div className="topnav">
+        <a href="/" className="logo">News Concept</a>
+          <a href="/">
+            <i class="fas fa-home"></i> Homepage
+          </a>
+          <a href="/articles">
+            <i class="fas fa-newspaper"></i> Articles
+          </a>
+          <a href="/topics">
+            <i class="fas fa-book"></i> Topics
+          </a>
+        </div>
+    </header>
   )
 }
 
