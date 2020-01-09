dfc201e20f5e5f86073f2c178bab6ba0bb0092c1
Ajinkya <ajinkya1394@outlook.com>
2020-01-09 03:46:37 +0000

    remove row div

--- src/components/views/Homepage.jsx
+++ src/components/views/Homepage.jsx
@@ -4,12 +4,10 @@ function Homepage() {
   return (
     <div className="homepage">
       <div className="container">
-        <div className="row">
-          <div className="homepage-content">
-            <div className="welcome-message">
-              <h3>Welcome to</h3>
-              <h1>News Concept</h1>
-            </div>
+        <div className="homepage-content">
+          <div className="welcome-message">
+            <h3>Welcome to</h3>
+            <h1>News Concept</h1>
           </div>
         </div>
       </div>
