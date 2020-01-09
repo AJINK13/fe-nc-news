7eb7c3dffd191849e6756d9b3dc3939d7b0cda03
Ajinkya <ajinkya1394@outlook.com>
2020-01-09 22:59:20 +0000

    change navbar component

--- src/components/views/NavBar.jsx
+++ src/components/views/NavBar.jsx
@@ -3,18 +3,17 @@ import React from "react"
 const NavBar = () => {
   return (
     <header>
-        <div className="topnav">
-        <a href="/" className="logo">News Concept</a>
-          <a href="/">
-            <i class="fas fa-home"></i> Homepage
-          </a>
-          <a href="/articles">
-            <i class="fas fa-newspaper"></i> Articles
-          </a>
-          <a href="/topics">
-            <i class="fas fa-book"></i> Topics
-          </a>
-        </div>
+      <div className="navbar">
+        <a href="/">
+          <i class="fas fa-home"></i> Homepage
+        </a>
+        <a href="/articles">
+          <i class="fas fa-newspaper"></i> Articles
+        </a>
+        <a href="/topics">
+          <i class="fas fa-book"></i> Topics
+        </a>
+      </div>
     </header>
   )
 }
