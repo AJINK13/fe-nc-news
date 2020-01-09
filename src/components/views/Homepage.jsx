8a25fb1ad7b99a4cac6d37168d92e3e2a72c29fc
Ajinkya <ajinkya1394@outlook.com>
2020-01-09 22:54:37 +0000

    change homepage ciew

--- src/components/views/Homepage.jsx
+++ src/components/views/Homepage.jsx
@@ -3,12 +3,7 @@ import React from "react"
 function Homepage() {
   return (
     <div className="homepage">
-      <div className="homepage-content">
-        <div className="welcome-message">
-          <h3>Welcome to</h3>
-          <h1>News Concept</h1>
-        </div>
-      </div>
+      <h1>News Concept</h1>
     </div>
   )
 }
